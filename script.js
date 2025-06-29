4418bc3271695e56c9d4c2119037a943a005450d
Kaninika-2309 <kaninikabanerjee3@gmail.com>
2025-06-29 16:49:44 +0000

    Added more effect

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const button=document.getElementById('toggle-theme');
+const typed= new Typed('#typed',{
+    strings:['Web Developer','Python learner'],
+    typespeed:60,
+    backspeed:40,
+    loop:true
+});
 
-button.addEventListener('click',() =>{
-    document.body.classList.toggle('dark mode');
+document.getElementById('toggle-theme').addEventListener('click' ,() =>{
+    document.body.classList.toggle('light')
 });
